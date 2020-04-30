5d7ed1dbede38e26c0c717cf61b2840fb28b2966
rodrigo-tinini <tinini.89@gmail.com>
2020-04-30 16:24:56 -0300

    Removing duplicate directory

--- 5GPy-Simulator/controlPlane.py
+++
@@ -1,9 +0,0 @@
-#this module presents a control plane for the network
-#the objective of this module is to centralize the network state and take centralize decisions
-
-#main control plane class
-class ControlPlane(object):
-    def __init__(self, aId, networkElements, networkGraph):
-        self.aId = aId
-        self.networkElements = networkElements
-        self.networkGraph = networkGraph
